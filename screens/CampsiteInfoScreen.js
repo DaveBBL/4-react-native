2f1d4d34e2eb37a06568b8f20901c41354b620f3
DaveBBL <dave.bbl@gmail.com>
2023-04-30 22:19:02 -0700

    Start RN wk 2

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import RenderCampsite from "../features/campsites/RenderCampsite";
+import { COMMENTS } from "../shared/comments";
 
 const CampsiteInfoScreen = ({ route }) => {
   const { campsite } = route.params;
-  return <RenderCampsite campsite={campsite} />;
+
+  const [comments, setComments] = useState(COMMENTS);
+
+  return (
+    <FlatList
+      data={comments.filter((comment) => comment.campsiteId === campsite.id)}
+    />
+  );
 };
 
 export default CampsiteInfoScreen;
